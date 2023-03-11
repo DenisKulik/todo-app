e92a73408ef567684f5dc8e5d8ebdc77b6a2f980
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-11 15:03:56 +0700

    add component templates

---
+++ src/components/Todos/TodoForm.js
@@ -0,0 +1,8 @@
+export default function TodoForm() {
+    return (
+        <form>
+            <input type="text" placeholder="Enter new todo"/>
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
