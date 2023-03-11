d37787fada2920b5286cc3c2235815a6685cbc52
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-11 16:53:52 +0700

    add a message about missing tasks

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -4,6 +4,7 @@ import styles from './TodoList.module.css';
 export default function TodoList({ todos }) {
     return (
         <div className={styles.todoListContainer}>
+            {!todos.length && <h2>Todo list is empty</h2>}
             {todos.map((todo, idx) => <Todo key={idx} todo={todo}/>)}
         </div>
     );
